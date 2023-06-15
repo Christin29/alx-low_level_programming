5f73384098eb6808125d7eb0d380962bd662e0b3
Christine Mueni <manjesta2817@gmail.com>
2023-06-15 12:35:15 -0700

    using put to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,11 @@
 #include<stdio.h>
-
 /**
-
- * main - Entry point
-
- *
-
+ * main- put instead of print 
+ * 
  * Return: Always 0 (Success)
-
- */
-
+ * */
 int main(void)
-
 {
-
         puts("\"Programming is like building a multilingual puzzle");
-
         return (0);
-
 }
-
-
-
