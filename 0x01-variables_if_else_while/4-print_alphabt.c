4b6cd4c7fbf28a4609cede77a3c1ee643d166f51
Christine Mueni <manjesta2817@gmail.com>
2023-06-16 09:27:42 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - excluding some alphabetic characters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+	int i;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		if (alp != 'q' && alp != 'e')
+	{
+		putchar(alp);
+	}
+	}
+	putchar('\n')
+	return (0);
+}
