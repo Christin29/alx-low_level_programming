6b6980ee30947ebeae974978649b929b51825b98
Christine Mueni <manjesta2817@gmail.com>
2023-06-16 10:17:26 -0700

    printing single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
+	return (0);
+}
