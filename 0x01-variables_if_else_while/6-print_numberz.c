b8fff211fe9f797ab5c36530898a1066972f2028
Christine Mueni <manjesta2817@gmail.com>
2023-06-16 10:35:24 -0700

    printing digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints digists without the char variables
+ *
+ * Return; Always 0 (success)
+ */
+int main(void)
+{
+	int 1;
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
