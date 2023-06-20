f9263dadeba0fd4a144ebc0f220eca799be58833
Christine Mueni <manjesta2817@gmail.com>
2023-06-20 10:25:14 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
- * main - print lowercase
+ * print_alphabet - print lowercase
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(alp[i]);
+		_putchar(i);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
