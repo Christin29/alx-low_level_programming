51fe7b7d6adc0a5dd418608743fdb4eb05fe4ec5
Christine Mueni <manjesta2817@gmail.com>
2023-06-20 05:51:16 -0700

    printing a string without using printf or puts

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<main.h>
+
+/**
+ * main - printing _putchar without using printf and puts
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+
+	putchar('\n');
+	return (0);
+}
