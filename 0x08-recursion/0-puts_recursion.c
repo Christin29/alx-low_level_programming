9ed0e7dd68fd61fcb31e5128997bf02fc9b49d0d
Christine Mueni <manjesta2817@gmail.com>
2023-07-06 06:03:28 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_puts_recursion - prints string recursively
+*@s: input string to print
+* Return: 0
+*/
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
+}
